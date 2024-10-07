6b25a1078f4802fdd6df23435056794aa0e2fa04
EthanStuck <ethanstuck@utexas.edu>
2024-10-07 02:18:17 -0700

    can travel to the 4 main scenes now

---
+++ world-quest/Scripts/forage.gd
@@ -0,0 +1,7 @@
+extends Node
+
+
+
+func _on_travel_back_area_entered(area: Area2D) -> void:
+	''' Travel back to town '''
+	get_tree().change_scene_to_file("res://Scenes/town.tscn")
---
+++ world-quest/Scripts/town.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var forage_scene = preload("res://Scenes/forage.tscn").instantiate()
+
+func _on_to_foraging_area_entered(area: Area2D) -> void:
+	''' Send player to foraging scene '''
+	get_tree().change_scene_to_file("res://Scenes/forage.tscn")
+
+func _on_to_fishing_area_entered(area: Area2D) -> void:
+	''' Send player to fishing scene '''
+	get_tree().change_scene_to_file("res://Scenes/fishing.tscn")
+
+func _on_to_fighting_area_entered(area: Area2D) -> void:
+	''' Send player to fighting scene '''
+	get_tree().change_scene_to_file("res://Scenes/fight.tscn")
+
+func _on_to_farming_area_entered(area: Area2D) -> void:
+	''' Send player to farming scene '''
+	get_tree().change_scene_to_file("res://Scenes/farm.tscn")
