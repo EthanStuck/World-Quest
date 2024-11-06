123d38059ed623c60283fbae815a9fbdf2ba514e
bchoi2004 <boazchoi27@gmail.com>
2024-11-06 15:11:21 -0600

    farm change

--- world-quest/Scripts/farm.gd
+++ world-quest/Scripts/farm.gd
@@ -8,7 +8,7 @@ func _on_travel_back_area_entered(area: Area2D) -> void:
 	get_tree().change_scene_to_file("res://Scenes/town.tscn")
 	
 func _process(delta):
-	if FragmentHandler.num_carrots >= 30 and not FragmentHandler.east_complete:
+	if FragmentHandler.num_carrots >= 15 and not FragmentHandler.east_complete:
 		var fragment_instance = fragment.instantiate()
 		get_parent().add_child(fragment_instance)
 		fragment_instance.global_position = $FragmentSpawnLocation.position
