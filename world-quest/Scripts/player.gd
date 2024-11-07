af087344733c45ecce3973bf76048874a9764056
EthanStuck <ethanstuck@utexas.edu>
2024-11-07 15:13:48 -0800

    fixed player and farming mechanics some

--- world-quest/Scripts/player.gd
+++ world-quest/Scripts/player.gd
@@ -12,7 +12,9 @@ var screen_size = Vector2(1000,1000)
 @export var currentHealth: int = maxHealth
 
 var dead = false
-
+var water_range = false
+var weed_range = false
+var interacting = false
 
 
 var foot_step = false
@@ -28,36 +30,45 @@ func _process(delta):
 	''' continuous processes '''
 	if not dead:
 		move(delta)
-		$Animations.z_index = position.y + 35
+		$Animations.z_index = position.y + 44
 		if Input.is_action_just_pressed('strike'):
 			strike(delta)
+		if Input.is_action_just_pressed('interact'):
+			if weed_range:
+				deweed()
+			elif water_range:
+				water()
 
 
 func move(delta):
 	''' controls player movement '''
 	
 	# player movement y direction
-	if Input.is_action_pressed('move_down'):
-		velocity.y = 1
-		if not Input.is_action_pressed('move_right') and not Input.is_action_pressed('move_left'):
-			$Animations.animation = 'walk_front'
-	elif Input.is_action_pressed('move_up'):
-		velocity.y = -1
-		if not Input.is_action_pressed('move_right') and not Input.is_action_pressed('move_left'):
-			$Animations.animation = 'walk_back'
+	if not interacting:
+		if Input.is_action_pressed('move_down'):
+			velocity.y = 1
+			if not Input.is_action_pressed('move_right') and not Input.is_action_pressed('move_left'):
+				$Animations.animation = 'walk_front'
+		elif Input.is_action_pressed('move_up'):
+			velocity.y = -1
+			if not Input.is_action_pressed('move_right') and not Input.is_action_pressed('move_left'):
+				$Animations.animation = 'walk_back'
+		else:
+			velocity.y = 0
+		
+		# player movement x direction
+		if Input.is_action_pressed('move_right'):
+			velocity.x = 1
+			if not Input.is_action_pressed('strike'):
+				$Animations.animation = 'walk_right'
+		elif Input.is_action_pressed('move_left'):
+			velocity.x = -1
+			if not Input.is_action_pressed('strike'):
+				$Animations.animation = 'walk_left'
+		else:
+			velocity.x = 0
 	else:
 		velocity.y = 0
-	
-	# player movement x direction
-	if Input.is_action_pressed('move_right'):
-		velocity.x = 1
-		if not Input.is_action_pressed('strike'):
-			$Animations.animation = 'walk_right'
-	elif Input.is_action_pressed('move_left'):
-		velocity.x = -1
-		if not Input.is_action_pressed('strike'):
-			$Animations.animation = 'walk_left'
-	else:
 		velocity.x = 0
 	
 	# normalize movement
@@ -69,7 +80,8 @@ func move(delta):
 	
 	else:
 		#$Animations.animation = 'idle'
-		$Animations.stop()
+		if not interacting:
+			$Animations.stop()
 	
 	# update position and control animation direction
 	#$Animations.flip_h = velocity.x < 0
@@ -82,20 +94,21 @@ func move(delta):
 		velocity = velocity.slide(collision.get_normal())
 
 func strike(delta):
-	$StrikeSound.play()
-	if velocity.x >= 0:
-		$Animations.animation = 'strike_right'
-		$HitBox/HitBoxShape.disabled = false
-		await get_tree().create_timer(.25).timeout
-		$HitBox/HitBoxShape.disabled = true
-		$Animations.animation = 'walk_right'
-	else:
-		#$Animations.flip_h = velocity.x < 0
-		$Animations.animation = 'strike_left'
-		$HitBox/HitBoxShape.disabled = false
-		await get_tree().create_timer(.25).timeout
-		$HitBox/HitBoxShape.disabled = true
-		$Animations.animation = 'walk_left'
+	if FragmentHandler.sword_pickup:
+		$StrikeSound.play()
+		if velocity.x >= 0:
+			$Animations.animation = 'strike_right'
+			$HitBox/HitBoxShape.disabled = false
+			await get_tree().create_timer(.25).timeout
+			$HitBox/HitBoxShape.disabled = true
+			$Animations.animation = 'walk_right'
+		else:
+			#$Animations.flip_h = velocity.x < 0
+			$Animations.animation = 'strike_left'
+			$HitBox/HitBoxShape.disabled = false
+			await get_tree().create_timer(.25).timeout
+			$HitBox/HitBoxShape.disabled = true
+			$Animations.animation = 'walk_left'
 
 func foot_step_sound():
 	''' make foot step sounds when walking '''
@@ -113,7 +126,41 @@ func collect(item):
 	inv.insert(item)
 
 
+func deweed():
+	interacting = true
+	var prev_anim = $Animations.animation
+	$Animations.animation = 'deweed_right'
+	await get_tree().create_timer(0.5).timeout
+	$Animations.animation = prev_anim
+	weed_range = false
+	interacting = false
+	
+func water():
+	interacting = true
+	var prev_anim = $Animations.animation
+	$Animations.stop()
+	$Animations.play('water_right')
+	await get_tree().create_timer(1).timeout
+	#$Animations.animation = prev_anim
+	$Animations.play(prev_anim)
+	water_range = false
+	interacting = false
+	
+func _on_interact_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group('weed'):
+		weed_range = true
 
+func _on_interact_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group('weed'):
+		weed_range = true
+
+func _on_interact_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group('dead_plant'):
+		water_range = true
+		
+func _on_interact_area_area_exited(area: Area2D) -> void:
+	if area.is_in_group('dead_plant'):
+		water_range = false
 
 func _on_hurt_box_area_entered(area: Area2D) -> void:
 	$HurtSound.play()
