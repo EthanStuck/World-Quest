66279c60210a4ee5129eb3d999ac3de027358636
EthanStuck <ethanstuck@utexas.edu>
2024-11-24 15:07:38 -0800

    saves progress in forage scene
    same janky implementation as farm scene

--- world-quest/Scripts/forage.gd
+++ world-quest/Scripts/forage.gd
@@ -15,12 +15,40 @@ func _ready():
 	spawn_tiny_phantom()
 	$YellowRocks/yellowrock8.hide()
 	$YellowRocks/yellowrock8/CollisionShape2D.disabled = true
+	total_pickup = ForageSave.num_rocks
 	
 	if FragmentHandler.south_spawned:
 		var fragment_instance = fragment.instantiate()
 		add_child.call_deferred(fragment_instance)
 		fragment_instance.global_position = $FragmentSpawnLocation.position
 		fragment_instance.collected.connect(on_fragment_collected)
+	
+	# recall which rocks have been collected
+	if ForageSave.rock1_collected:
+		$YellowRocks/yellowrock1.queue_free()
+	if ForageSave.rock2_collected:
+		$YellowRocks/yellowrock2.queue_free()
+	if ForageSave.rock3_collected:
+		$YellowRocks/yellowrock3.queue_free()
+	if ForageSave.rock4_collected:
+		$YellowRocks/yellowrock4.queue_free()
+	if ForageSave.rock5_collected:
+		$YellowRocks/yellowrock5.queue_free()
+	if ForageSave.rock6_collected:
+		$YellowRocks/yellowrock6.queue_free()
+	if ForageSave.rock7_collected:
+		$YellowRocks/yellowrock7.queue_free()
+	if ForageSave.rock8_collected:
+		$YellowRocks/yellowrock8.queue_free()
+	if ForageSave.rock9_collected:
+		$YellowRocks/yellowrock9.queue_free()
+	if ForageSave.rock10_collected:
+		$YellowRocks/yellowrock10.queue_free()
+	if ForageSave.rock11_collected:
+		$YellowRocks/yellowrock11.queue_free()
+	if ForageSave.rock12_collected:
+		$YellowRocks/yellowrock12.queue_free()
+
 
 func _on_travel_back_area_entered(area: Area2D) -> void:
 	''' Travel back to town '''
@@ -86,6 +114,7 @@ func _on_forage_npc_give_item() -> void:
 
 func _on_yellowrock_collected() -> void:
 	total_pickup += 1
+	ForageSave.num_rocks += 1
 	
 	if total_pickup == 10 and not given:
 		confused.emit()
@@ -101,3 +130,30 @@ func _on_yellowrock_collected() -> void:
 			FragmentHandler.south_complete = true
 			FragmentHandler.south_spawned = true
 			fragment_instance.collected.connect(on_fragment_collected)
+
+
+# save which rocks have been collected
+func _on_yellowrock1_collected() -> void:
+	ForageSave.rock1_collected = true
+func _on_yellowrock_2_collected() -> void:
+	ForageSave.rock2_collected = true
+func _on_yellowrock_3_collected() -> void:
+	ForageSave.rock3_collected = true
+func _on_yellowrock_4_collected() -> void:
+	ForageSave.rock4_collected = true
+func _on_yellowrock_5_collected() -> void:
+	ForageSave.rock5_collected = true
+func _on_yellowrock_6_collected() -> void:
+	ForageSave.rock6_collected = true
+func _on_yellowrock_7_collected() -> void:
+	ForageSave.rock7_collected = true
+func _on_yellowrock_8_collected() -> void:
+	ForageSave.rock8_collected = true
+func _on_yellowrock_9_collected() -> void:
+	ForageSave.rock9_collected = true
+func _on_yellowrock_10_collected() -> void:
+	ForageSave.rock10_collected = true
+func _on_yellowrock_11_collected() -> void:
+	ForageSave.rock11_collected = true
+func _on_yellowrock_12_collected() -> void:
+	ForageSave.rock12_collected = true
--- world-quest/forage_save.gd
+++ world-quest/forage_save.gd
@@ -1,1 +1,19 @@
 extends Node
+''' This stores progress in the forage scene'''
+
+# if rock has been collected
+var rock1_collected = false
+var rock2_collected = false
+var rock3_collected = false
+var rock4_collected = false
+var rock5_collected = false
+var rock6_collected = false
+var rock7_collected = false
+var rock8_collected = false
+var rock9_collected = false
+var rock10_collected = false
+var rock11_collected = false
+var rock12_collected = false
+
+# num rocks collected
+var num_rocks = 0
